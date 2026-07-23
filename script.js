38048093eb5ce55ff06adfe6741ed74ef6196061
ninjabloxi <oneplus7ninjabloxi@gmail.com>
2026-07-23 16:02:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// ======================================
-// FIFA TV - Firebase Authentication
-// script.js
-// ======================================
-
-
 import { initializeApp } from "firebase/app";
 
 import {
@@ -20,76 +14,46 @@ import {
 
 
 
-// ======================================
-// Firebase config depuis Vercel / .env
-// ======================================
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA0wGD-T7bzHGuSu7qxw7qEe-tExpqz0p4",
-  authDomain: "fifa-tv-database.firebaseapp.com",
-  projectId: "fifa-tv-database",
-  storageBucket: "fifa-tv-database.firebasestorage.app",
-  messagingSenderId: "916734081043",
-  appId: "1:916734081043:web:13a741a8747ace5d3ee7e6",
-  measurementId: "G-P89G4BDBWX"
+
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 
+console.log(firebaseConfig);
 
-// Vérification configuration
-
-console.log(
-    "Firebase API KEY :",
-    firebaseConfig.apiKey
-);
-
-
-
-// ======================================
-// Initialize Firebase
-// ======================================
 
 
 const app = initializeApp(firebaseConfig);
 
 
-
-// Analytics (optionnel)
-
-let analytics = null;
 
 try {
 
-    analytics = getAnalytics(app);
+    getAnalytics(app);
 
-}
-
-catch(error){
+} catch(e) {
 
     console.log(
-        "Analytics désactivé"
+        "Analytics indisponible"
     );
 
 }
 
-
-
-// ======================================
-// Auth
-// ======================================
 
 
 const auth = getAuth(app);
@@ -100,21 +64,12 @@ new GoogleAuthProvider();
 
 
 
-// ======================================
-// Exports
-// ======================================
-
-
 export {
 
     auth,
-
     googleProvider,
-
     signInWithPopup,
-
     onAuthStateChanged,
-
     signOut
 
 };
